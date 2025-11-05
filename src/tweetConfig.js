69a0112109d312d53866084d63d40f673609cbab
Jared Wheet <jaredwheet@gmail.com>
2025-11-04 18:55:17 -0600

    account updates

--- src/tweetConfig.js
+++ src/tweetConfig.js
@@ -1,15 +1,15 @@
 // Twitter handles to tag in every tweet for engagement
 // Add or remove handles as needed
 export const TAGGED_ACCOUNTS = [
-//   '@DukeFootball',
-//   '@DukeChronicle',
-//   '@DukeAthletics',
-//   '@DukeFootballrdt',
-//   '@Section17Media',
-//   '@ga_scumbags'
+  '@DukeFootball',
+  '@DukeChronicle',
+  '@DukeAthletics',
+  '@DukeFootballrdt',
+  '@Section17Media',
+  '@ga_scumbags'
 ];
 // Hashtags to include in every tweet for discoverability
 export const HASHTAGS = [
-//   '#DukeFootball',
-//   '#DUKEFBSCORIGAMI'
+  '#DukeFootball',
+  '#DUKEFBSCORIGAMI'
 ];
